149f5c08db893704ab10d82129f7e97c70ebdb0d
Enlith Zhou <enlith@gmail.com>
2016-11-21 22:50:51 +0100

    Update Python Function Docstring

--- FuncAllParameters.py
+++ FuncAllParameters.py
@@ -1,4 +1,9 @@
 def allparameter(part1, part2=1, *numbers, **keywords):
+    '''example code to show all kinds of parameter that python could use. 
+       normal parameter like part1,
+       default value parameter like part2,
+       varing length parameters like numbers
+       and hash list like keywords'''
     for i in range(1,part1):
         print (i-1)*'\t','looping part1:[{0}]'.format(i)
         for j in range(1,part2):
@@ -9,4 +14,4 @@ def allparameter(part1, part2=1, *numbers, **keywords):
                 print (j-1)*'\t',key, keywords[key]
 
 allparameter(2, 3, 1, 3, 5, 7, 9, Dec=12, Nov=11, Oct=10)
-
+print allparameter.__doc__
