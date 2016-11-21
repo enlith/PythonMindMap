673628d0c6f2771e33bcef3c036990afe6cfe9d1
Enlith Zhou <enlith@gmail.com>
2016-11-21 22:40:48 +0100

    Update Python Function part

---
+++ FuncAllParameters.py
@@ -0,0 +1,12 @@
+def allparameter(part1, part2=1, *numbers, **keywords):
+    for i in range(1,part1):
+        print (i-1)*'\t','looping part1:[{0}]'.format(i)
+        for j in range(1,part2):
+            print (j-1)*'\t','looping part2:[{0}]'.format(j)
+            for n in numbers:
+                print (j-1)*'\t',n
+            for key in keywords:
+                print (j-1)*'\t',key, keywords[key]
+
+allparameter(2, 3, 1, 3, 5, 7, 9, Dec=12, Nov=11, Oct=10)
+
